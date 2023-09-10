45d0a1f86070117e6aa11cb36ff04eeff030a0f6
Jeferson Luis Alves de Souza <jsouza@ibm.com>
2023-09-10 13:16:58 -0300

    add imagens da boticario

---
+++ src/api/data.js
@@ -0,0 +1,50 @@
+const imagensInfo = [
+    {
+      id: 1,
+      titulo: "En solo 3 pasos",
+      subTitulo: "Gana 1 iPhone 14",
+      buttonName: "¡CONOCE MÁS!",
+      link: "https://www.oboticario.com.co/cdn/shop/files/Home-Banner-Desk_3dac5de7-a4c6-4992-b268-eb879b50df24.jpg?v=1693525890&width=3000",
+      text: "Actividad válida del 01 al 30 de septiembre de 2023. Consulta los T&C completos de la actividad en https://www.oboticario.com.co/pages/concurso-iphone-14.",
+      type: '/Promociones'
+    },
+    {
+      id: 2,
+      titulo: "¡NUEVO! Egeo Choc Berry y Egeo Choc Mint",
+      subTitulo: "Lleva ambos con 25%",
+      buttonName: "¡LO QUIERO!",
+      link: "https://www.oboticario.com.co/cdn/shop/files/DesktopMeca-1-Egeo_d4e85f07-6692-4986-811d-d4e2a92fa338.jpg?v=1693439331&width=3000",
+      text: "*Referencias Seleccionadas*No acumulable *Hasta agotar existencias*Imagen de referencia. Fecha inicio 31/08/2023 Fecha fin 2/10/2023",
+      type: '/Egeo'
+    },
+    {
+      id: 3,
+      titulo: "Amor y Amistad es OBoticário",
+      subTitulo: "Regalos desde 49.900",
+      buttonName: "¡APROVECHA HOY!",
+      link: "https://www.oboticario.com.co/cdn/shop/files/DesktopMeca-2-Namor_9bb935d2-63d9-481a-84d4-299ff66a92df.jpg?v=1693520994&width=3000",
+      text: "*Referencias Seleccionadas*No acumulable *Hasta agotar existencias*Imagen de referencia. Fecha inicio 31/08/2023 Fecha fin 2/10/2023",
+      type: '/Regalos'
+    },
+    {
+      id: 4,
+      titulo: "BOTIK Tecnología Suiza. Resultados rápidos y",
+      subTitulo: "Combos hasta 25% OFF",
+      buttonName: "¡LO NECESITO!",
+      link: "https://www.oboticario.com.co/cdn/shop/files/DesktopMeca-3-Botik.jpg?v=1693439680&width=3000",
+      text: "*Referencias Seleccionadas*No acumulable *Hasta agotar existencias*Imagen de referencia. Fecha inicio 31/08/2023 Fecha fin 2/10/2023",
+      type: '/Promociones'
+    },
+    {
+      id: 5,
+      titulo: "¡Perfumes para todos!",
+      subTitulo: "Hasta 25% OFF",
+      buttonName: "¡APROVECHA YA!",
+      link: "https://www.oboticario.com.co/cdn/shop/files/Home-Banner-Desk_a44e66ed-9599-41e9-9a49-cc75b727e5c5.jpg?v=1694199414&width=3000",
+      text: "*Referencias Seleccionadas*No acumulable *Hasta agotar existencias*Imagen de referencia. Fecha inicio 31/08/2023 Fecha fin 2/10/2023",
+      type: '/Promociones'
+    },
+  ];
+  
+export default imagensInfo;
+  
--- src/components/views/home/Slide.tsx
+++ src/components/views/home/Slide.tsx
@@ -2,51 +2,13 @@
 import "@/styles/Slide.global.css";
 import Image from "next/image";
 import React, { useState, useEffect } from "react";
+import imagens from "@/api/data"; // Importa o array de dados do arquivo externo
+import Link from "next/link";
+
+
 
 export default function Slide() {
-  const imagens = [
-    {
-      id: 1,
-      titulo: "En solo 3 pasos",
-      subTitulo: "Gana 1 iPhone 14",
-      buttonName: "¡CONOCE MÁS!",
-      link: "/imagens/celular.webp",
-      text: "Actividad válida del 01 al 30 de septiembre de 2023. Consulta los T&C completos de la actividad en https://www.oboticario.com.co/pages/concurso-iphone-14.",
-    },
-    {
-      id: 2,
-      titulo: "¡NUEVO! Egeo Choc Berry y Egeo Choc Mint",
-      subTitulo: "Lleva ambos con 25%",
-      buttonName: "¡LO QUIERO!",
-      link: "/imagens/egeo.webp",
-      text: "*Referencias Seleccionadas*No acumulable *Hasta agotar existencias*Imagen de referencia. Fecha inicio 31/08/2023 Fecha fin 2/10/2023",
-    },
-    {
-      id: 3,
-      titulo: "Amor y Amistad es OBoticário",
-      subTitulo: "Regalos desde 49.900",
-      buttonName: "¡APROVECHA HOY!",
-      link: "/imagens/celebre.webp",
-      text: "*Referencias Seleccionadas*No acumulable *Hasta agotar existencias*Imagen de referencia. Fecha inicio 31/08/2023 Fecha fin 2/10/2023",
-    },
-    {
-      id: 4,
-      titulo: "BOTIK Tecnología Suiza. Resultados rápidos y",
-      subTitulo: "Combos hasta 25% OFF",
-      buttonName: "¡LO NECESITO!",
-      link: "/imagens/acido.webp",
-      text: "*Referencias Seleccionadas*No acumulable *Hasta agotar existencias*Imagen de referencia. Fecha inicio 31/08/2023 Fecha fin 2/10/2023",
-    },
-    {
-      id: 5,
-      titulo: "¡Perfumes para todos!",
-      subTitulo: "Hasta 25% OFF",
-      buttonName: "¡APROVECHA YA!",
-      link: "/imagens/lily.webp",
-      text: "*Referencias Seleccionadas*No acumulable *Hasta agotar existencias*Imagen de referencia. Fecha inicio 31/08/2023 Fecha fin 2/10/2023",
-    },
-  ];
-
+  
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [progress, setProgress] = useState(0);
 
@@ -121,14 +83,12 @@ export default function Slide() {
         </div>
         <div className="slide-row">
           <div className="slide-col">
-            <a
-              role="slide-button-image"
+            <Link role="slide-button-text" href={imagens[currentImageIndex]["type"]}
               key={imagens[currentImageIndex]["id"]}
-              href="#"
               className="slide-button"
-            >
+              >
               {imagens[currentImageIndex]["buttonName"]}
-            </a>
+            </Link>
           </div>
         </div>
 
