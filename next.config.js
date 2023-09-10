1851bf8aad6bcc742efb851ab0ccce5c68513cda
Jeferson Luis Alves de Souza <jsouza@ibm.com>
2023-09-10 12:12:43 -0300

    adicionando uma novo dominio

--- next.config.js
+++ next.config.js
@@ -2,8 +2,14 @@ const withImages = require('next-images');
 
 module.exports = withImages({
   images: {
-    domains: ['oboticario.com.co', 'boticario-desafio.onrender.com'],
+    domains: ['www.oboticario.com.co', 'boticario-desafio.onrender.com'],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     formats: ['image/webp'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 });
