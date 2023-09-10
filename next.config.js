ef88cfec1ce3fb683bef6118a4b75b22cce3575c
Jeferson Luis Alves de Souza <jsouza@ibm.com>
2023-09-10 12:34:14 -0300

    add dominio

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withImages = require('next-images');
 
 module.exports = withImages({
   images: {
-    domains: ['www.oboticario.com.co', 'boticario-desafio.onrender.com'],
+    domains: ['www.oboticario.com.co', 'desafio-oboticario.onrender.com'],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     formats: ['image/webp'],
     remotePatterns: [
